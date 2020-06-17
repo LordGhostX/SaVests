34ed8c9dc9a3fbaa0859a64b7c4cdd45c95e2740
LordGhostX <solomonghost9@gmail.com>
2020-06-17 13:15:58 +0100

    added db stats

--- SaVests/dashboard/models.py
+++ SaVests/dashboard/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+class Users(models.Model):
+    name = models.CharField(max_length=100)
+    active = models.IntegerField(default=1)
+    date = models.DateTimeField(default=datetime.now, blank=True)
+
+    def __str__(self):
+        return self.name
--- SaVests/dashboard/views.py
+++ SaVests/dashboard/views.py
@@ -1,7 +1,19 @@
+from datetime import timedelta
+from datetime import datetime
 from django.shortcuts import render
 from django.contrib.admin.views.decorators import staff_member_required
+from django.db.models import F
+from .models import *
 
 # Create your views here.
 @staff_member_required
 def index(request):
-    return render(request, "index.html")
+    data = {
+        "within_24_hours": len(Users.objects.filter(date__gt=datetime.now() - timedelta(days=1))),
+        "within_past_week": len(Users.objects.filter(date__gt=datetime.now() - timedelta(days=7))),
+        "within_past_month": len(Users.objects.filter(date__gt=datetime.now() - timedelta(days=30))),
+        "total_users": len(Users.objects.all()),
+        "active_users": len(Users.objects.filter(active=1)),
+        "inactive_users": len(Users.objects.filter(active=0))
+    }
+    return render(request, "index.html", data)
