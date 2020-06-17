e6b9fa1aa39eb04299fe6f260d7fe769914a8713
LordGhostX <solomonghost9@gmail.com>
2020-06-17 12:15:47 +0100

    admin redirection

--- SaVests/dashboard/views.py
+++ SaVests/dashboard/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.contrib.admin.views.decorators import staff_member_required
 
 # Create your views here.
+@staff_member_required
+def index(request):
+    return render(request, "index.html")
